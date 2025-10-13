b789a4b2ed0b0182b86ec8bfd3acda6f72ab8798
MBANS8A1 <seanmbng@aol.com>
2025-10-13 19:04:56 +0100

    I modified the css for the PackingList component and added borders around the ul li elements, so I can understand how the layout responds when new luggage items are added.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./index.css";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Body Lotion", quantity: 1, packed: false },
 ];
 
 function App() {
@@ -22,9 +23,9 @@ function Logo() {
 
 function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>Form elements for your 😍 trip</h3>
-    </div>
+    </form>
   );
 }
 
@@ -43,11 +44,10 @@ function PackingList() {
 function Item({ item }) {
   return (
     <li>
-      <span>
-        {item.quantity}
-        {item.description}
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button className="cross">❌</button>
     </li>
   );
 }
