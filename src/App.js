83918585b1bd2a1159b20b87ad5b3735669d7f84
MBANS8A1 <seanmbng@aol.com>
2025-10-19 09:59:03 +0100

    I fixed a bug that would have made the number of items selected from the select option dropdown list in the Form element remain a String datatype. I made the input and select dynamic dropdown list controlled elements.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./index.css";
-
+import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -23,14 +23,18 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(5);
 
   function handleSubmit(e) {
     e.preventDefault();
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for you 😍 trip?</h3>
-      <select>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
