4708f41bf6fddfe8137d5c804795a9b7d6389965
MBANS8A1 <seanmbng@aol.com>
2025-10-20 12:48:49 +0100

    I added some more CSS to the Form component ADD button so its semantic meaning is clear.

--- src/App.js
+++ src/App.js
@@ -30,7 +30,6 @@ function Form() {
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
-
     setDescription("");
     setQuantity(1);
   }
